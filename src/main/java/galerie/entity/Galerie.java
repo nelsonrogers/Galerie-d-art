22bf4b81d33bcf3bb37a1225453f2d2ce6e6a63c
Nelson Rogers <nelsonlrrogers@gmail.com>
2021-01-06 15:53:16 +0100

    initial commit

--- src/main/java/galerie/entity/Galerie.java
+++ src/main/java/galerie/entity/Galerie.java
@@ -3,6 +3,7 @@ import javax.persistence.*;
 import lombok.*;
 
 // Un exemple d'entité
+//
 // On utilise Lombok pour auto-générer getter / setter / toString...
 // cf. https://examples.javacodegeeks.com/spring-boot-with-lombok/
 @Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
